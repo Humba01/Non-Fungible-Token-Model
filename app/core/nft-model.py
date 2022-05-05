89fbadc3f8963c095ff1d2bf149708abb121a797
Humba01 <humbandroid@gmail.com>
2022-05-05 01:04:33 -0300

    Revisitando o projeto.

--- app/core/nft-model.py
+++ app/core/nft-model.py
@@ -4,3 +4,23 @@ import os, sys
 def definicao_nft_model(nome_nft, nome_modelo, serial_nft):
   """ Função que define o modelo da NF-T. """
 
+# A definição de um blockchain
+def definicao_blockchain_model(nome_blockchain, nome_modelo, serial_blockchain):
+  """ Função que define o modelo do blockchain. """
+
+# A definição de um modelo de banco de dados
+def definicao_modelo_bd(nome_modelo, nome_bd, nome_tabela, nome_coluna):
+  """ Função que define o modelo do banco de dados. """
+
+# Cria o modelo do banco de dados
+# modelo_bd = definicao_modelo_bd(nome_modelo, nome_bd, nome_tabela, nome_coluna)
+
+# Cria o modelo da NFT
+# nft_model = definicao_nft_model(nome_nft, nome_modelo, serial_nft)
+
+# Cria o modelo do blockchain
+# blockchain_model = definicao_blockchain_model(nome_blockchain, nome_modelo, serial_blockchain)
+
+# Cria uma rede de dados
+# rede_dados = definicao_rede_dados(nome_rede, nome_modelo, nome_bd, nome_tabela, nome_coluna)
+
