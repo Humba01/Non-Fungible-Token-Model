7d0d5841ab1a644ee3dbc061b1b527b05688e2ff
Humba01 <humbandroid@gmail.com>
2022-07-24 20:13:25 +0000

    Arrumando projeto.

---
+++ app/nft-model.py
@@ -0,0 +1,10 @@
+# A definição de uma Non-Fungible Token (NFT)
+def criar_nft(nft_id, nft_owner, nft_name, nft_description, nft_image):
+  nft_information = { 
+    "id": nft_id,
+    "owner": nft_owner, 
+    "name": nft_name,
+    "description": nft_description,
+    "image": nft_image 
+  }
+  return nft_information
